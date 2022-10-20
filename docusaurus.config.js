60ab62f3a521354255370ef1f720da948a1966e6
ChenWenWu223 <wenwuchen1948@gmail.com>
2022-10-20 14:55:20 +0800

    blog update: 2022/10/20 周四 14:55:18.40

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   title: "Cww's Blog",
   tagline: "Today is a present",
   url: "https://chenwenwu223.github.io",
-  baseUrl: "/",
+  baseUrl: "/blog/",
   baseUrlIssueBanner: true,
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
