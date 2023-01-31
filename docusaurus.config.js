2fccab270c1be97814c56351c58ae1e2830c76cd
chenwenwu <chenwenwu@kanyun.com>
2023-01-31 11:26:56 +0800

    feat: update docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   title: "Winwoo's Blog",
   tagline: "Today is a present.",
   url: "https://chenwenwu223.github.io",
-  baseUrl: "/blog/",
+  baseUrl: "/",
   baseUrlIssueBanner: true,
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
